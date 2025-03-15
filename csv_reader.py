28ab5a70351c7751dde952a6524b8236765c33ed
iziefigueiredo <figueiredo_isadora@hotmail.com>
2025-03-15 19:23:18 -0300

    organizar

--- csv_reader.py
+++ csv_reader.py
@@ -2,7 +2,9 @@ import csv
 
 class CSVReader:
     """
+    
     Classe para carregar e analisar arquivos CSV.
+    
     """
 
     def __init__(self, filename='steam.csv'):
@@ -10,14 +12,22 @@ class CSVReader:
 
     def load_data(self):
         """
+        
         Carrega os dados do arquivo CSV e retorna uma lista de dicionários.
+       
         """
         try:
             with open(self.filename, mode='r', encoding='utf-8') as csvfile:
                 reader = csv.DictReader(csvfile)
                 games = [row for row in reader]
             return games
+        
+        
         except FileNotFoundError as e:
+            
             raise FileNotFoundError(f"Arquivo não encontrado: {self.filename}") from e
+  
+    
         except Exception as e:
+            
             raise Exception(f"Erro ao ler o arquivo CSV: {e}") from e
